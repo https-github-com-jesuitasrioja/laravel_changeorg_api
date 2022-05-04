1ef13a2880c08e1a550210e139964aed72193850
ilafdez <ineslafdez@gmail.com>
2022-05-04 10:49:10 +0200

    docu peticiones

--- app/Http/Controllers/PeticionesController.php
+++ app/Http/Controllers/PeticionesController.php
@@ -179,13 +179,59 @@ class PeticionesController extends Controller
      *              type="integer"
      *          )
      *      ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Mostrar la petición."
-     *     ),
-     *     @OA\Response(
-     *         response="default",
-     *         description="Ha ocurrido un error."
+     *     *      @OA\Response(
+     *        response="200",
+     *        description="Successful response",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                property="peticion",
+     *                type="array",
+     *                example={{"id":1,"titulo":"lalala3","descripcion":"lalala3","destinatario":"lalala3","firmantes":0,"estado":"pendiente","user_id":1,"categoria_id":1,"image":null,"created_at":"2022-05-03T07:47:11.000000Z","updated_at":"2022-05-03T07:50:31.000000Z"},{"id":11,"titulo":"lalala2","descripcion":"lalala2","destinatario":"lalala2","firmantes":0,"estado":"pendiente","user_id":1,"categoria_id":1,"image":null,"created_at":"2022-05-03T07:47:22.000000Z","updated_at":"2022-05-03T07:47:22.000000Z"},{"id":21,"titulo":"lalala3","descripcion":"lalala3","destinatario":"lalala3","firmantes":0,"estado":"pendiente","user_id":1,"categoria_id":1,"image":null,"created_at":"2022-05-03T08:06:02.000000Z","updated_at":"2022-05-03T08:06:02.000000Z"}},
+     *                @OA\Items(
+     *                      @OA\Property(
+     *                         property="id",
+     *                         type="number",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="titulo",
+     *                         type="string",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="descripcion",
+     *                         type="string",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="destinatario",
+     *                         type="string",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="firmantes",
+     *                         type="number",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="estado",
+     *                         type="string",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="user_id",
+     *                         type="number",
+     *                         example=""
+     *                      ),
+     *                      @OA\Property(
+     *                         property="categoria_id",
+     *                         type="number",
+     *                         example=""
+     *                      ),
+     *                ),
+     *             ),
+     *        ),
      *     )
      * )
      */
