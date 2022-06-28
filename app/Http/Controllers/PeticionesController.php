b2401db2f2d8293251ca7cc28bd1f10299a56c35
ilafdez <ineslafdez@gmail.com>
2022-06-28 09:28:40 +0200

    cambios

--- app/Http/Controllers/PeticionesController.php
+++ app/Http/Controllers/PeticionesController.php
@@ -234,8 +234,12 @@ class PeticionesController extends Controller
         //$id = 2;
         $peticiones = Peticione::all(); //->where('user_id', $user->id);
 
-        return $peticiones->toJson(JSON_OBJECT_AS_ARRAY);
+        // return $peticiones->toJson(JSON_OBJECT_AS_ARRAY);
         //return "[{'kk':'kk'}]"; //$peticiones;
+        $peticiones = Peticione::all();
+
+        return $peticiones;
+
     }
 
     /**
